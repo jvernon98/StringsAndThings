5e1ca902b1883fc0f0d5c0153946589f6fb2ebc1
Jennifer <jennifer@Zipcoders-MacBook-Pro-9.local>
2023-03-02 18:53:01 -0500

    I have added changed

--- src/main/java/io/zipcoder/StringsAndThings.java
+++ src/main/java/io/zipcoder/StringsAndThings.java
@@ -15,7 +15,16 @@ public class StringsAndThings {
      *           countYZ("day fyyyz"); // Should return 2
      */
     public Integer countYZ(String input){
-        return null;
+
+        int count = 0; //begin the count 0
+        String[] sentences = input.split(" "); //creating a new string with the words in 'input' split by spaces
+        for (int i=0; i<sentences.length; i++) //create a variable that will stand for the numbers its counting up
+        //it will increase the variable by increments of 1, so the code looks at each separately
+        if(input.endsWith("y")||input.endsWith("z")){ //so if those broken up words end with y or z it increases the
+            // count by one increment
+            count++;
+        }
+        return count;
     }
 
     /**
@@ -28,7 +37,13 @@ public class StringsAndThings {
      *           removeString("Hello there", "x") // Should return "Hello there"
      */
     public String removeString(String base, String remove){
-        return null;
+
+        return base.replace(remove,"");
+        //the replace method searches a string for specified characters and returns a new string where
+        //the specified characters are replaced
+        //the string in the first position (base) identifies the characters or words that will be replaced
+        //the string in the second position (remove) identifies what to replace, but since we're not
+        //actually replacing the letters we're taking out, we leave it blank, thus "" empty quotes
     }
 
     /**
@@ -40,8 +55,12 @@ public class StringsAndThings {
      *           containsEqualNumberOfIsAndNot("noisxxnotyynotxisi") // Should return true
      */
     public Boolean containsEqualNumberOfIsAndNot(String input){
-        return null;
+        return input.split("is",-1).length == input.split("not",-1).length;
     }
+    //the split turns the three strings in "input" into arrays, since strings are immutable
+    //it will then search for all instances of "is" or "not" and the -1 tells it to cycle through
+    //the arrays as many times as possible. the .length tells it how long it is
+    //"==" is equal to the same code except with "not" being split, and if they are equal it returns true
 
     /**
      * We'll say that a lowercase 'g' in a string is "happy" if there is another 'g' immediately to its left or right.
@@ -50,11 +69,15 @@ public class StringsAndThings {
      *           gHappy("xxgxx") // Should return  false
      *           gHappy("xxggyygxx") // Should return  false
      */
-    public Boolean gIsHappy(String input){
-        return null;
+    public Boolean gIsHappy(String input) {
+        String Happy = input;
+        for (int i=0; i<Happy.length(); i++) {
+            if (Happy.charAt(i) == 'g' && Happy.charAt(i + 1) == 'g') {
+                return true;
+            }
+        }
+    return false;
     }
-
-
     /**
      * We'll say that a "triple" in a string is a char appearing three times in a row.
      * Return the number of triples in the given string. The triples may overlap.
@@ -62,7 +85,13 @@ public class StringsAndThings {
      *            countTriple("xxxabyyyycd") // Should return 3
      *            countTriple("a") // Should return 0
      */
-    public Integer countTriple(String input){
-        return null;
-    }
-}
+    public Integer countTriple(String input) {
+        int count = 0;
+
+        for (int i = 0; i <= input.length() - 3; i++) {
+            if (input.charAt(i) == input.charAt(i + 1) &&
+                    input.charAt(i) == input.charAt(i + 2))
+                count++;
+        }
+        return count;
+    }}
